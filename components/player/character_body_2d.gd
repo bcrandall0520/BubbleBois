7a6c1d686348afd8ef3e226ec8d1b8c8f25f9004
bcrandall0520 <bcrandall0520@gmail.com>
2025-01-25 13:25:40 -0700

    Fast Falling

--- components/player/character_body_2d.gd
+++ components/player/character_body_2d.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 1000.0
 const JUMP_VELOCITY = -1000.0
-
+const FAST_FALL_VELOCITY = 1000.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,7 +13,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("player_1_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("player_1_fast_fall") and !is_on_floor():
+		velocity.y = FAST_FALL_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("player_1_left", "player_1_right")
